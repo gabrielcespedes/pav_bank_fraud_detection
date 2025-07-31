483144d6bfef312dede682642c809dd0b302dd74
Gabriel Céspedes <gabrielcespedesalarc@gmail.com>
2025-07-31 10:23:42 -0400

    reubicacion de script de prediccion.html

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -8,4 +8,17 @@ function login() {
     } else {
         alert("Por favor, introduce tanto el usuario como la contraseña."); 
     }
-}
+};
+
+// Mostrar el nombre del archivo seleccionado en prediccion.html
+document.addEventListener('DOMContentLoaded', function () {
+    const fileInput = document.getElementById('file');
+    const fileNameDisplay = document.getElementById('file-name');
+
+    if (fileInput && fileNameDisplay) {
+        fileInput.addEventListener('change', function () {
+            const fileName = this.files[0] ? this.files[0].name : 'Ningún archivo seleccionado';
+            fileNameDisplay.textContent = fileName;
+        });
+    }
+});
